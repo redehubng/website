b26403f93b7f55d02cf9f1eb77d4048f27fc5768
smartmikey <donsmartez@gmail.com>
2018-12-19 00:53:56 +0100

    tawk

--- js/tawk.js
+++ js/tawk.js
@@ -1,7 +1,5 @@
-<
-script type = "text/javascript" >
-    var Tawk_API = Tawk_API || {},
-        Tawk_LoadStart = new Date();
+var Tawk_API = Tawk_API || {},
+    Tawk_LoadStart = new Date();
 (function() {
     var s1 = document.createElement("script"),
         s0 = document.getElementsByTagName("script")[0];
