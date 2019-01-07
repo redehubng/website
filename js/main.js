5965ae7e2a23510546d6439965304e742cf1d0f5
solinkz <solomoneyitene@gmail.com>
2019-01-07 13:57:02 +0100

    did fix on navbar for mobile

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@
 
 window.onscroll = function() { scrollFunction() };
 
-function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.getElementById("topnav").style.background = "rgba(0,0,0,0.8)";
-        document.getElementById("topnav").style.borderBottom = "3px solid red";
-        // document.getElementById("logo").style.fontSize = "25px";
-    } else {
-        document.getElementById("topnav").style.backgroundColor = "rgba(255,255,255,0)";
-        document.getElementById("logo").style.fontSize = "35px";
-    }
-}
+// function scrollFunction() {
+//     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//         document.getElementById("topnav").style.background = "rgba(0,0,0,0.8)";
+//         document.getElementById("topnav").style.borderBottom = "3px solid red";
+//         // document.getElementById("logo").style.fontSize = "25px";
+//     } else {
+//         document.getElementById("topnav").style.backgroundColor = "rgba(255,255,255,0)";
+//         document.getElementById("logo").style.fontSize = "35px";
+//     }
+// }
 
 //owl carousel function
 $(document).ready(function() {
